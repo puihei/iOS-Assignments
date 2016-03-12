49eca394b946229728316d72de7c94bad764190f
puihei <ccheung@me.com>
2016-03-12 12:22:38 +0800

    initial commit test

---
+++ myTestCodeProject/myTestCodeProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myTestCodeProject
+//
+//  Created by Chris Cheung on 12/3/2016.
+//  Copyright © 2016 Chris Cheung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
